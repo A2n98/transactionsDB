a600a60298401fd1c140d62516248fd9a077bda9
A2n98 <GizatullinaAnna00@gmail.com>
2021-12-22 23:03:43 +0500

    Красиво упаковала файлы

---
+++ src/parse/Parser.java
@@ -0,0 +1,56 @@
+package parse;
+import classes.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Parser {
+
+    public List<Transaction> ParseTransactionsCsv(String filePath) throws IOException {
+        var transactions = new ArrayList<Transaction>();
+        var fileLines = Files.readAllLines(Paths.get(filePath));
+
+        for (String fileLine : fileLines) {
+            var lineArr = ParseLine(fileLine);
+            if (Objects.equals(lineArr.get(0), "Series_reference")) continue;
+
+            var trInfo = new TransInfo(lineArr.get(3), lineArr.get(4), lineArr.get(5), lineArr.get(6), lineArr.get(7), lineArr.get(8));
+            var seTitles = new SerieTitles(lineArr.get(9), lineArr.get(10), lineArr.get(11), lineArr.get(12), lineArr.get(13));
+            var doub = Objects.equals(lineArr.get(2), "") ? 0.0 : Double.parseDouble(lineArr.get(2));
+
+            var transaction = new Transaction(lineArr.get(0), lineArr.get(1), doub, trInfo, seTitles);
+
+            transactions.add(transaction);
+        }
+        return transactions;
+    }
+
+    private ArrayList<String> ParseLine(String line){
+        var res = new ArrayList<String>();
+        StringBuilder value = new StringBuilder();
+        for(var b: line.toCharArray()){
+            if(Objects.equals(b,',')){
+                if (value.length() == 0){
+                    res.add("");
+                }
+                else{
+                    res.add(value.toString());
+                }
+                value = new StringBuilder();
+            }
+            else{
+                value.append(b);
+            }
+        }
+
+        if (res.size() != 14){
+            while (res.size() != 14) res.add("");
+        }
+
+        return res;
+    }
+}
