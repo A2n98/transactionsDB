6efd7616093fc5799a2cfb67f1181517269073c5
A2n98 <GizatullinaAnna00@gmail.com>
2021-12-24 18:10:06 +0500

    help!!!

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import parse.*;
+import date_base.*;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         var filePath = "./transactions.csv";
         var parser = new Parser();
         var transList = parser.ParseTransactionsCsv(filePath);
-        System.out.println(transList.get(0));
+        var db = DBworker.getInstance();
+        //for(var transaction:transList){
+            //db.addTransaction(transaction);
+        //}
 
     }
 }
---
+++ src/date_base/DBworker.java
@@ -0,0 +1,61 @@
+package date_base;
+
+import classes.Transaction;
+import org.sqlite.JDBC;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBworker {
+
+    // Константа, в которой хранится адрес подключения
+    private static final String CON_STR = "jdbc:sqlite:transactions.sqlite";
+
+    // Используем шаблон одиночка, чтобы не плодить множество
+    // экземпляров класса DbHandler
+    private static DBworker instance = null;
+
+    public static synchronized DBworker getInstance() throws SQLException {
+        if (instance == null)
+            instance = new DBworker();
+        return instance;
+    }
+
+    // Объект, в котором будет храниться соединение с БД
+    private Connection connection;
+
+    private DBworker() throws SQLException {
+        // Регистрируем драйвер, с которым будем работать
+        // в нашем случае Sqlite
+        DriverManager.registerDriver(new JDBC());
+        // Выполняем подключение к базе данных
+        this.connection = DriverManager.getConnection(CON_STR);
+    }
+
+    public void addTransaction(Transaction transaction){
+        try (var statement = this.connection.prepareStatement(
+                "INSERT INTO 'Transactions'('Series_reference','Period','Data_value','Suppressed','STATUS','UNITS'," +
+                        "'Magnitude','Subject','Group','Series_title_1','Series_title_2','Series_title_3','Series_title_4','Series_title_5') " +
+                        "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)")) {
+            statement.setObject(1, transaction.serieRef);
+            statement.setObject(2, transaction.period);
+            statement.setObject(3, transaction.dataValue);
+            statement.setObject(4, transaction.information.suppressed);
+            statement.setObject(5, transaction.information.status);
+            statement.setObject(6, transaction.information.units);
+            statement.setObject(7, transaction.information.magnitude);
+            statement.setObject(8, transaction.information.subject);
+            statement.setObject(9, transaction.information.group);
+            statement.setObject(10, transaction.serieTitles.title1);
+            statement.setObject(11, transaction.serieTitles.title2);
+            statement.setObject(12, transaction.serieTitles.title3);
+            statement.setObject(13, transaction.serieTitles.title4);
+            statement.setObject(14, transaction.serieTitles.title5);
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
